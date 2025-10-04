538cb78949e4a1390e8db54c6d312ea070223844
GarrellMac <gmacarilay1@gmail.com>
2025-10-04 09:16:23 +0800

    Fixing bug in popup.js jquery

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
+import './assets/css/templatemo-chain-app-dev.css'
+import './assets/css/owl.css'
+import './assets/css/fontawesome.css'
+import './assets/css/animated.css'
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
